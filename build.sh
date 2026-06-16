c99da93a7dbb48975566832b85ded6fb826dcf0c
Matt Gumbley <matt.gumbley@devzendo.org>
2026-06-16 22:15:30 +0100

    Correct build script.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run -it --rm -v /run/host-services/ssh-auth.sock:/ssh-agent -e SSH_AUTH_SOCK="/ssh-agent" -v $UPONE:$UPONE -w $PWD --name documentation documentation make
+docker run -it --rm -v /run/host-services/ssh-auth.sock:/ssh-agent -e SSH_AUTH_SOCK="/ssh-agent" -w $PWD --name documentation documentation make
 
 
